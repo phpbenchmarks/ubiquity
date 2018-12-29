0c5581fa7a99b25c80740a71262a9ab5faff8b6f
jcheron <myaddressmail@gmail.com>
2018-12-29 02:31:02 +0100

    Define mvcNS rest part
    to test

--- app/config/config.php
+++ app/config/config.php
@@ -6,6 +6,6 @@ return array(
 		"debug"=>false,
 		"di"=>["translator"=>function(){ return new \Ubiquity\translation\Translator("fr_FR","en");}],
 		"cache"=>["directory"=>"cache/","system"=>\Ubiquity\cache\system\ArrayCache::class,"params"=>[]],
-		"mvcNS"=>["models"=>"models","controllers"=>"../vendor/phpbenchmarks/ubiquity-common/controllers","rest"=>""],
+		"mvcNS"=>["models"=>"models","controllers"=>"../vendor/phpbenchmarks/ubiquity-common/controllers","rest"=>"../vendor/phpbenchmarks/ubiquity-common/controllers/rest"],
 		"isRest"=>true
 );
